00dddc3777ef9b96024ad541db9b76b818abd077
Andressa Benedette <andressa@lumosweb.com.br>
2023-11-27 16:12:08 -0300

    SPA Universe

---
+++ Stage 06/SPA Universe/js/router.js
@@ -0,0 +1,39 @@
+export class Router {
+  routes = {}
+
+  add(routeName, page) {
+    this.routes[routeName] = page
+  }
+
+  route(event) {
+    event = event || window.event
+    event.preventDefault()
+
+    window.history.pushState({}, "", event.target.href)
+
+    this.handle()
+  }
+
+  handle() {
+    const { pathname } = window.location
+    const route = this.routes[pathname] || this.routes[404]
+
+    fetch(route)
+      .then((data) => data.text())
+      .then((html) => {
+        document.querySelector("#app").innerHTML = html
+      })
+
+    this.handleNavActive(pathname)
+  }
+
+  handleNavActive(path) {
+    const navActive = document.querySelector(`a[href="${path}"]`)
+    const navAll = document.querySelectorAll(`nav a`)
+
+    for (const button of navAll) {
+      button.classList.remove("active")
+    }
+    navActive.classList.add("active")
+  }
+}
